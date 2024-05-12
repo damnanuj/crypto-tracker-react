58b62e3f3e4b9c69cc71db26fcc12b3a05c5c991
Anuj <anujkrgupta21@gmail.com>
2024-05-12 17:11:24 +0530

    first commit - setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Footer from './components/Common/Footer';
+import Header from './components/Common/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Common/Header/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./style.scss"
+const Header = () => {
+  return (
+    <div className='header'>
+      header
+    </div>
+  )
+}
+
+export default Header
