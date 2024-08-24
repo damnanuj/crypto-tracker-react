7b01483bbc9fcf2e3046d244919a7140a944bacf
Anuj <anujkrgupta21@gmail.com>
2024-08-24 15:01:14 +0530

    animation

--- src/components/Dashboard/Grid/GridComponent.js
+++ src/components/Dashboard/Grid/GridComponent.js
@@ -3,6 +3,7 @@ import "./grid.scss";
 import { Tooltip } from "@mui/material";
 import { formatNumber } from "../../../functions/numberFormatter";
 import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
 
 const GridComponent = ({ coin }) => {
   const cardStyle = `${
@@ -19,36 +20,43 @@ const GridComponent = ({ coin }) => {
 
   return (
     <Link to={`/coin/${coin.id}`}>
-    <div className={`grid-card ${cardStyle}`}>
-      <div className="name-logo">
-        <img src={coin.image} className="coinImg" alt={coin.name} />
-        <div className="symbol-name">
-          <h3 className="name">{coin.name}</h3>
-          <p className="symbol">{coin.symbol}</p>
+      <motion.div
+        className={`grid-card ${cardStyle}`}
+        initial={{ opacity: 0, y: 50 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.4 }}
+      >
+        <div className="name-logo">
+          <img src={coin.image} className="coinImg" alt={coin.name} />
+          <div className="symbol-name">
+            <h3 className="name">{coin.name}</h3>
+            <p className="symbol">{coin.symbol}</p>
+          </div>
+          <Tooltip title="Add to watchlist">
+            <span className={`material-icons favorite ${favorite}`}>
+              favorite
+            </span>
+          </Tooltip>
         </div>
-        <Tooltip title="Add to watchlist">
-        <span className={`material-icons favorite ${favorite}`}>favorite</span>
-        </Tooltip>
-      </div>
-      <div className="percentContainer">
-        <p className={percentageStyle}>{coin.price_change_percentage_24h}%</p>
-        {/* TODO:icon */}
-        {price >= 0 ? (
-          <span className="material-icons priceUp">trending_up</span>
-        ) : (
-          <span className="material-icons priceDown">trending_down</span>
-        )}
-      </div>
-      <h2 className={priceStyle}>${coin.current_price}</h2>
-      <div className="totalVolume">
-        <h5>
-          Total Volume: <span>${formatNumber(coin.total_volume)}</span>{" "}
-        </h5>
-        <h5>
-          Market Cap: <span>${formatNumber(coin.market_cap)}</span>
-        </h5>
-      </div>
-    </div>
+        <div className="percentContainer">
+          <p className={percentageStyle}>{coin.price_change_percentage_24h}%</p>
+          {/* TODO:icon */}
+          {price >= 0 ? (
+            <span className="material-icons priceUp">trending_up</span>
+          ) : (
+            <span className="material-icons priceDown">trending_down</span>
+          )}
+        </div>
+        <h2 className={priceStyle}>${coin.current_price}</h2>
+        <div className="totalVolume">
+          <h5>
+            Total Volume: <span>${formatNumber(coin.total_volume)}</span>{" "}
+          </h5>
+          <h5>
+            Market Cap: <span>${formatNumber(coin.market_cap)}</span>
+          </h5>
+        </div>
+      </motion.div>
     </Link>
   );
 };
--- src/components/Dashboard/List/ListComponent.js
+++ src/components/Dashboard/List/ListComponent.js
@@ -3,6 +3,7 @@ import "./list.scss";
 import { Tooltip } from "@mui/material";
 import { formatNumber } from "../../../functions/numberFormatter";
 import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
 
 const ListComponent = ({ coin }) => {
   const cardStyle = `${
@@ -19,57 +20,68 @@ const ListComponent = ({ coin }) => {
 
   return (
     <Link to={`/coin/${coin.id}`}>
-    <tr className={`list-card ${cardStyle}`}>
-      {/* Logo and name */}
-      <Tooltip title="coin logo & name" >
-        <td>
-          <div className="name-logo list-name-logo">
-            <img src={coin.image} className="coinImg listImg" alt="logo" />
-            <div className="symbol-name">
-              <h3 className="name listName">{coin.name}</h3>
-              <p className="symbol listSymbol">{coin.symbol}</p>
+      <motion.tr
+        className={`list-card ${cardStyle}`}
+        initial={{ opacity: 0, x: -50 }}
+        whileInView={{ opacity: 1, x: 0 }}
+        transition={{ duration: 0.6}}
+      >
+        {/* Logo and name */}
+        <Tooltip title="coin logo & name">
+          <td>
+            <div className="name-logo list-name-logo">
+              <img src={coin.image} className="coinImg listImg" alt="logo" />
+              <div className="symbol-name">
+                <h3 className="name listName">{coin.name}</h3>
+                <p className="symbol listSymbol">{coin.symbol}</p>
+              </div>
             </div>
-          </div>
-          
-        </td>
-      </Tooltip>
-          {/* percentage change */}
-      <Tooltip title="percentage change" >
-        <td>
-          <div className="percentContainer">
-            <p className={percentageStyle}>
-              {coin.price_change_percentage_24h}%
-            </p>
-            {/* TODO:icon */}
-            {price >= 0 ? (
-              <span className="material-icons priceUp row-icon">trending_up</span>
-            ) : (
-              <span className="material-icons priceDown row-icon">trending_down</span>
-            )}
-          </div>
-        </td>
-      </Tooltip>
-
-      <Tooltip title={`cuurent price: $${coin.current_price}`}>
-        <td>
-          <h2 className={priceStyle}>${formatNumber(coin.current_price)}</h2>
-        </td>
-      </Tooltip>
-      <Tooltip title="Total volume">
-        <td className="row-total-vol">
-          <h5 className="vol ">${formatNumber(coin.total_volume)}</h5>
-        </td>
-      </Tooltip>
-      <Tooltip title="Market Cap">
-        <td className="row-market-cap">
-          <h5 className="cap">${formatNumber(coin.market_cap)}</h5>
-        </td>
-      </Tooltip>
-      <Tooltip title="Add to watchlist">
-        <td>
-        <span className={`material-icons favorite ${favorite}`}>favorite</span></td>
+          </td>
         </Tooltip>
-    </tr>
+        {/* percentage change */}
+        <Tooltip title="percentage change">
+          <td>
+            <div className="percentContainer">
+              <p className={percentageStyle}>
+                {coin.price_change_percentage_24h}%
+              </p>
+              {/* TODO:icon */}
+              {price >= 0 ? (
+                <span className="material-icons priceUp row-icon">
+                  trending_up
+                </span>
+              ) : (
+                <span className="material-icons priceDown row-icon">
+                  trending_down
+                </span>
+              )}
+            </div>
+          </td>
+        </Tooltip>
+
+        <Tooltip title={`cuurent price: $${coin.current_price}`}>
+          <td>
+            <h2 className={priceStyle}>${formatNumber(coin.current_price)}</h2>
+          </td>
+        </Tooltip>
+        <Tooltip title="Total volume">
+          <td className="row-total-vol">
+            <h5 className="vol ">${formatNumber(coin.total_volume)}</h5>
+          </td>
+        </Tooltip>
+        <Tooltip title="Market Cap">
+          <td className="row-market-cap">
+            <h5 className="cap">${formatNumber(coin.market_cap)}</h5>
+          </td>
+        </Tooltip>
+        <Tooltip title="Add to watchlist">
+          <td>
+            <span className={`material-icons favorite ${favorite}`}>
+              favorite
+            </span>
+          </td>
+        </Tooltip>
+      </motion.tr>
     </Link>
   );
 };
